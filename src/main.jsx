c842b28edfd6e6d906bd7360b779600c07fbe41a
Piyush Rajesh Deshkar <shacx8@gmail.com>
2026-06-09 12:22:46 +0530

    Import CSS file and adjust Dashboard import path

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Dashboard from "./dashboard.jsx";
+import "./index.css";
+import Dashboard from "./dashboard";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
